a5a6b75afcf2957920d6858e3121bab753185f2b
Rohith Ravut <rohith.ravut@dell.com>
2025-05-16 11:48:49 +0530

    Update test_xcat_services.py
    Signed-off-by: Rohith Ravut <rohith.ravut@dell.com>

--- molecule/prepare_oim/tests/test_xcat_services.py
+++ molecule/prepare_oim/tests/test_xcat_services.py
@@ -1,34 +1,8 @@
-import subprocess
 import pytest
-import sys
-import os
-import json
 
-# Add project root to path to import config
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
-import config
-
-# Configuration
-oim_ip = config.OIM_IP
-password = config.OIM_PASS
 CONTAINER_NAME = "omnia_provision"
 
-def run_sshpass_command(cmd):
-    """
-    SSH into the remote host and execute 'podman exec' to get the software_config.json file.
-    """
-    remote_cmd = cmd
-    
-    ssh_command = [
-        "sshpass", "-p", password,
-        "ssh", "-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null",
-        f"root@{oim_ip}", remote_cmd
-    ]
-
-    result = subprocess.run(ssh_command, capture_output=True, text=True)
-    return result
-
-def test_xcatd_installation():
+def test_xcatd_installation(run_sshpass_command):
     """
     SSH into the remote host and execute 'podman exec' to run lsxcatd -v inside the container.
     """
@@ -38,7 +12,7 @@ def test_xcatd_installation():
     assert result.returncode == 0, f"\nxcat is not installed. \nError:\n{result.stderr}"
     print("\nxcat is installed!")
         
-def test_xcatd_services():
+def test_xcatd_services(run_sshpass_command):
     """
     SSH into the remote host and execute 'podman exec' to run lsxcatd -v inside the container.
     """
@@ -48,7 +22,7 @@ def test_xcatd_services():
     assert result.returncode == 0, f"\nxcatd services are not running.\nError:\n{result.stderr}"
     print("\nxcatd services are running")
 
-def test_xcatd_status():
+def test_xcatd_status(run_sshpass_command):
 
     cmd = f"podman exec -it {CONTAINER_NAME} systemctl is-active xcatd"
     
@@ -56,7 +30,7 @@ def test_xcatd_status():
     print("\nxcat status: ",xcat_status)
     assert xcat_status == "active\n", f"Expected xcat to be active, but got {xcat_status}"
 
-def test_xcat_perl_modules():
+def test_xcat_perl_modules(run_sshpass_command):
     
     tabdump = ["site", "nodetype", "nodelist", "networks"]
     cmd_failed = []
