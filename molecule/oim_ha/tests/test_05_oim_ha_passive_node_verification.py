ec85ea9d52c3f24f2b1c05179c7498c181b76da9
Rohith Ravut <rohith.ravut@dell.com>
2025-06-30 23:18:05 +0530

    Update test_05_oim_ha_passive_node_verification.py
    Signed-off-by: Rohith Ravut <rohith.ravut@dell.com>

--- molecule/oim_ha/tests/test_05_oim_ha_passive_node_verification.py
+++ molecule/oim_ha/tests/test_05_oim_ha_passive_node_verification.py
@@ -1,11 +1,20 @@
+# Copyright 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import pytest
 
 container_name = "omnia_core"
-# omnia_pcs_passive_nodes = False
-# omnia_pcs_daemon_passive_nodes = False
-# omnia_pcs_resources_passive_nodes = False
-# omnia_pcs_online_node_list_passive_nodes = False
-
 
 @pytest.mark.dependency(name='test_omnia_pcs_passive_nodes')
 def test_omnia_pcs_passive_nodes(run_sshpass_command, get_oim_ha_nodes, check_if_oim_ha_is_enabled):
