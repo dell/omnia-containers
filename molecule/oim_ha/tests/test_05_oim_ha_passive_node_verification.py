0e66a6b4d6d20eb76ab3bb72bb6bf8e90d911c5b
Rohith Ravut <rohith.ravut@dell.com>
2025-07-01 10:33:07 +0530

    Update test_05_oim_ha_passive_node_verification.py
    Signed-off-by: Rohith Ravut <rohith.ravut@dell.com>

--- molecule/oim_ha/tests/test_05_oim_ha_passive_node_verification.py
+++ molecule/oim_ha/tests/test_05_oim_ha_passive_node_verification.py
@@ -47,7 +47,7 @@ def test_omnia_pcs_passive_nodes_TC_3699(run_sshpass_command, get_oim_ha_nodes, 
     except Exception as e:
         pytest.fail(f"Error in test_omnia_pcs_passive_nodes: {str(e)}")
 
-@pytest.mark.dependency(name='test_omnia_pcs_passive_nodes')
+@pytest.mark.dependency(name="test_pcs_daemon_passive_nodes", depends=["test_omnia_pcs_passive_nodes"])
 def test_pcs_daemon_passive_nodes_TC_3699(run_sshpass_command, get_oim_ha_nodes, check_pcs_daemon_status):
     print("\nVerifying PCS daemon status on passive nodes")
     global omnia_pcs_daemon_passive_nodes
@@ -70,7 +70,7 @@ def test_pcs_daemon_passive_nodes_TC_3699(run_sshpass_command, get_oim_ha_nodes,
     except Exception as e:
         pytest.fail(f"Error in test_pcs_daemon_passive_nodes: {str(e)}")
 
-@pytest.mark.dependency(name='test_omnia_pcs_passive_nodes')
+@pytest.mark.dependency(name="test_pcs_resources_passive_nodes", depends=["test_omnia_pcs_passive_nodes"])
 def test_pcs_resources_passive_nodes_TC_3699(run_sshpass_command, get_required_pcs_resources, check_pcs_resource_status, get_hostname, get_oim_ha_nodes):
     print("\nVerifying PCS resources on passive nodes")
     global omnia_pcs_resources_passive_nodes
@@ -106,7 +106,7 @@ def test_pcs_resources_passive_nodes_TC_3699(run_sshpass_command, get_required_p
     except Exception as e:
         pytest.fail(f"Error in test_pcs_resources_passive_nodes: {str(e)}")
 
-@pytest.mark.dependency(name='test_omnia_pcs_passive_nodes')
+@pytest.mark.dependency(name="test_online_node_list_passive_node", depends=["test_omnia_pcs_passive_nodes"])
 def test_online_node_list_passive_node_TC_3699(run_sshpass_command, parse_online_nodes, get_oim_ha_nodes, get_hostname):
     print("\nVerifying online node status from passive nodes")
     global omnia_pcs_online_node_list_passive_nodes
