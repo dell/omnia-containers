a0e04865e730f3bdf3c8fc49f33afab0b471c899
prasant750h <prasanth.charagenti@dellteam.com>
2025-08-20 11:00:00 +0530

    Update remote_utils.py
    Signed-off-by: prasant750h <prasanth.charagenti@dellteam.com>

--- code/remote_utils.py
+++ code/remote_utils.py
@@ -1,6 +1,20 @@
 # remote_utils.py
+# Copyright 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# Copyright (c) 2025 Your Name or Company
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-import subprocess
 """
 This module provides utility functions to perform remote operations via SSH and SCP.
 It includes functionality to:
@@ -12,22 +26,19 @@ Note: This script uses `sshpass`, which exposes passwords on the command line.
 It's intended for quick automation tasks in trusted environments only.
 """
 
-def copy_file_to_remote(user, password, ip, local_path, remote_path):
-      """
-    Copy a file from the local machine to a remote machine using SCP.
+import subprocess
 
+def copy_file_to_remote(user, password, ip, local_path, remote_path):
+    """
+    Copy a file from the local machine to a remote machine using SCP.
     Args:
         user (str): Username for SSH login.
         password (str): Password for SSH login (used by sshpass).
         ip (str): IP address of the remote host.
         local_path (str): Path to the local file.
         remote_path (str): Destination path on the remote host.
-
     Returns:
         tuple: (return_code, stdout, stderr)
-            return_code (int): 0 if successful, -1 if an error occurred.
-            stdout (str): Standard output from the command.
-            stderr (str): Standard error from the command or exception message.
     """
     scp_command = [
         "sshpass", "-p", password,
@@ -51,20 +62,15 @@ def copy_file_to_remote(user, password, ip, local_path, remote_path):
         return -1, "", str(e)
 
 def run_remote_command(user, password, ip, cmd):
-        """
+    """
     Run a shell command on a remote machine via SSH.
-
     Args:
         user (str): Username for SSH login.
         password (str): Password for SSH login (used by sshpass).
         ip (str): IP address of the remote host.
         cmd (str): Shell command to execute on the remote host.
-
     Returns:
         tuple: (return_code, stdout, stderr)
-            return_code (int): 0 if successful, -1 if an error occurred.
-            stdout (str): Output from the command.
-            stderr (str): Error output or exception message.
     """
     ssh_command = [
         "sshpass", "-p", password,
@@ -87,18 +93,14 @@ def run_remote_command(user, password, ip, cmd):
         return -1, "", str(e)
 
 def check_node_reachability(ip, count=3, timeout=5):
-     """
+    """
     Check if a node (IP address) is reachable using the ping command.
-
     Args:
         ip (str): IP address of the node to check.
-        count (int): Number of ping packets to send (default is 3).
-        timeout (int): Timeout in seconds for each ping packet (default is 5).
-
+        count (int): Number of ping packets to send.
+        timeout (int): Timeout in seconds for each ping packet.
     Returns:
         tuple: (reachable, output)
-            reachable (bool): True if the node is reachable, False otherwise.
-            output (str): The ping output or error message.
     """
     ping_command = ["ping", "-c", str(count), "-W", str(timeout), ip]
 
