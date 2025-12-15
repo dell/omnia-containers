133ad4161d08f834cee88b2ef5532684cd7c9e7e
Abhishek S A <abhishek.sa3@dell.com>
2025-12-15 16:24:54 +0530

    Update build_images.sh
    Signed-off-by: Abhishek S A <abhishek.sa3@dell.com>

--- build_images.sh
+++ build_images.sh
@@ -193,12 +193,12 @@ BUILD_ACTION="load"
 OMNIA_DOCKER_REGISTERY="docker.io/dellhpcomniaaisolution"
 
 # Default image tags for each container (can be overridden individually)
-CORE_TAG="latest"
-AUTH_TAG="latest"
-PCS_TAG="latest"
-UBUNTU_LDMS_TAG="latest"
+CORE_TAG="1.0"
+AUTH_TAG="1.0"
+PCS_TAG="1.0"
+UBUNTU_LDMS_TAG="1.0"
 # Global fallback tag (used when image_tag= is specified)
-IMAGE_TAG="latest"
+IMAGE_TAG="1.0"
 
 # Valid parameter names
 VALID_PARAMS=("omnia_branch" "build_tool" "build_action" "image_tag" "core_tag" "auth_tag" "pcs_tag" "ubuntu_ldms_tag")
