b3f908a8d054fbaddbc27ad136b1525ec4541756
balajikumaran-c-s <balajikumaran.cs@dellteam.com>
2025-12-23 18:14:42 +0530

    Add Apache 2.0 license headers to all source files
    - Added Dell Apache 2.0 license headers to 49 Python files
    - Added license headers to 13 molecule YAML files
    - Added license headers to 2 shell script files
    - Fixed import paths in omnia_sh and prepare_oim modules
    - Fixed directory creation bug in repository.py download function
    - Updated HTML report labels from 'Pipeline' to 'Test Run'
    - All files now comply with Dell licensing standards

--- automation_library/omnia_sh/messages/omnia_sh_msgs.py
+++ automation_library/omnia_sh/messages/omnia_sh_msgs.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Omnia.sh - Messages and Test Variables.
 
@@ -7,7 +21,7 @@ and test variables for the omnia.sh automation.
 
 from typing import Dict
 from ..vars.omnia_sh_vars import OMNIA_SH_VARS
-from ...vars import OIM_PREREQ_VARS
+from ...checks.vars.oim_prereq_vars import OIM_PREREQ_VARS
 
 # =============================================================================
 # TEST VARIABLES (for molecule/pytest tests)
--- automation_library/prepare_oim/vars/prepare_oim_vars.py
+++ automation_library/prepare_oim/vars/prepare_oim_vars.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Prepare OIM - Configuration Variables.
 
@@ -14,7 +28,7 @@ import json
 import os
 from typing import Dict, Any, List
 
-from automation_library.vars.oim_prereq_vars import OIM_PREREQ_VARS
+from automation_library.checks.vars.oim_prereq_vars import OIM_PREREQ_VARS
 
 
 def _get_project_root() -> str:
