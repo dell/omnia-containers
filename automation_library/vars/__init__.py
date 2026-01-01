1a7bb684c31541fc3bc6b8c545fc023bfbdaf8ab
ria.renold@dellteam.com <ria.renold@dellteam.com>
2026-01-01 12:57:32 +0530

    Cleanup: Removed old slurm files from automation_library and molecule folders

--- automation_library/vars/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Variables module for automation library.
-
-Re-exports commonly used variables for convenient imports.
-"""
-
-from automation_library.checks.vars.oim_prereq_vars import OIM_PREREQ_VARS, USER_CONFIG_PATH
-
-__all__ = ["OIM_PREREQ_VARS", "USER_CONFIG_PATH"]
--- molecule/insufficient_resources/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for insufficient_resources scenario."""
